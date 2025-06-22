f481f35270c67a8df92ea2f7dc0d0e90b1348cd1
Chin Wey Sheng <mystic@Mystics-MacBook-Pro.local>
2025-06-23 03:30:07 +0800

    cws added style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,6 +18,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import './index.css';
+import './style.css';
 
 const App = () => {
   return (
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './style.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
